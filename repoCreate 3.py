235acef034b1cc7c7899085f44a84f0007a7196c
MD <altair2400@gmail.com>
2023-02-23 21:58:01 +0100

    hello

--- repoCreate 3.py
+++ repoCreate 3.py
@@ -1,32 +1,40 @@
 import subprocess
-
 from copier import run_auto
 
+def main():
+    repocreate()
 
-class MainClass:
+def repocreate(project_name, full_name= "Jacob_Hector", email = "jacob.hector@ufst.dk", repo_and_artifact_name = "Nørrebro"):
+    print("Initializing git repository..")
+    deleteCommand = "rm -rf desktop/tmp/pe"
+    delteComRes = subprocess.run(deleteCommand, capture_output=True, shell=True)
+    print(delteComRes.stdout.decode()) 
+    
+    directoryCreate = "mkdir desktop/tmp/pe"
+    ret1 = subprocess.run(directoryCreate, capture_output=True, shell=True)
+    print(ret1.stdout.decode())
+    
+    copyRepo = "python3 -m copier --defaults -d project_name=gitHubProjekt2023 copy git@github.ccta.dk:w33412/CreateRepo.git desktop/tmp/pe"
+    print(subprocess.run(copyRepo, shell=True))
 
-    def repocreate(project_name, full_name= "Jacob_Hector", email = "jacob.hector@ufst.dk", repo_and_artifact_name = "Nørrebro"):
 
-        deleteCommand = "rm -rf desktop/tmp/pe"
-        delteComRes = subprocess.run(deleteCommand, capture_output=True, shell=True)
-        print(delteComRes.stdout.decode())
-        txt1 = ["python3 -m copier --defaults -d project_name=gitHubProjekt2023 copy git@github.ccta.dk:w33412/CreateRepo.git desktop/tmp/pe"]
-        print(subprocess.run(txt1, shell=True))
-
-        command = "cd /desktop/tmp/pe; git init -b main; git add --all; git commit -m 'hello'; git remote add origin git@github.ccta.dk:w33412/RepoTest.git; git push -u origin main; git push;"
-        ret = subprocess.run(command, capture_output=True, shell=True)
-        print(ret.stdout.decode())
-
-
-    #     print("Initializing git repository..")
+    directoryChange = "cd desktop/tmp/pe; git init -b main; git add --all; git commit -m 'hello'; git remote add origin git@github.ccta.dk:w33412/RepoTest.git; git push -u origin main;"
+    ret = subprocess.run(directoryChange, capture_output=True, shell=True)
+    print(ret.stdout.decode())
+    subprocess.run(copyRepo, shell=True)
+    
+    
+    subprocess.check_call(["git remote add origin https://git@github.ccta.dk:w33412/hello.git"],shell=True)
+    
+        
     #     subprocess.check_output(["rm", "-rf", "/desktop/tmp/pe"])
     #     run_auto('git@github.ccta.dk:w33412/CreateRepo.git', "/desktop/tmp/pe" + txt1)
     #     subprocess.call('python3 -m copier --defaults \
     #     -d project_name=CreateRepo \
-    #   copy git@github.ccta.dk:devops/toolchain-springboot-ref-template.git /tmp/local_copy')
+    #     copy git@github.ccta.dk:devops/toolchain-springboot-ref-template.git /tmp/local_copy')
     #
     # """
-    #     subprocess.run(run_auto("eit@eithub.ceta.dk:433412/CreateRepo.glt/desktop/tmp/pe")
+    #     subprocess.run(run_auto("git@github.ceta.dk:w33412/CreateRepo.glt/desktop/tmp/pe")
     #     subprocess.call(python3 -m copier --defaults \
     #     -d project_name="<CreateRepo>" \
     #     copy git@github.ccta.dk:devops/toolchain-springboot-ref-template.git /tmp/local_copy
@@ -46,5 +54,13 @@ class MainClass:
     # subprocess.check_call([ copier copy -d . run auto ("git@github.ccta.dk:w33412/create-repo-test-clone.git" )],
     #                       subprocess. Popen ("1s", CWd = "/devops-springboot-ref")
 
-    if __name__ == '__main__':
-        repocreate("sucuk", "", "", "")
+
+def testCode():
+    testCommnad = " cd desktop/tmp/pe; pwd"
+    ret = subprocess.run(testCommnad, shell=True)
+    print(ret.stdout.decode())
+
+
+if __name__ == "__main__":
+    #repocreate("", "", "", "")
+    testCode()
